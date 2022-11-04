4ef38012d9c1a229c647088f83e772a494097f53
Romeo <romeogescolano@gmail.com>
2022-11-04 22:05:48 +0800

    add: add the instruction of the assignment by comment

--- name.py
+++ name.py
@@ -1,3 +1,7 @@
+# Assignment 1:
+# Create a program that will print your nickname using only asterisk character (*)
+# Your program should be uploaded to github. 
+
 print("* * *        *  *  *      *         *    * * * * *     *  *  *   ")
 print("*     *    *         *    *  *   *  *    *           *         * ")
 print("*     *    *         *    *    *    *    *           *         * ")
